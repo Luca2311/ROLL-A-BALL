380ca3fc988006aedae098e446fc0e803bafe3ae
Lucija Safaric <lucija.safaric2311@gmail.com>
2020-04-20 16:28:31 +0200

    igra 3d
    20.4.

---
+++ Roll-a-Ball/Assets/Skripte/KameraController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KameraController : MonoBehaviour
+{
+
+    public GameObject player;
+    private Vector3 pomak;
+    private void Start()
+    {
+        pomak = transform.position - player.transform.position;
+    }
+
+    private void LateUpdate()
+    {
+        transform.position = player.transform.position + pomak;
+    }
+}
---
+++ Roll-a-Ball/Assets/Skripte/Kretanje.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Kretanje : MonoBehaviour
+{
+
+    //Deklaracija varijable za brzinu
+    public Text countText;
+    public float speed;
+    private int zbroj;
+    private Rigidbody rb;
+  
+
+    private void Start()
+    {
+        //dohvacanje 
+        rb = GetComponent<Rigidbody>();
+        count = 0;
+        SetCountText();
+    }
+
+    private void FixedUpdate()
+    {
+        //dohvacanje vrijednosti osi
+        float kretanjeHorizontal = Input.GetAxis("Horizontal");
+        float kretanjeVertical = Input.GetAxis("Vertical");
+
+        //postavljanje
+        Vector3 kretanje = new Vector3(kretanjeHorizontal, 0.0f, kretanjeVertical);
+
+        
+        //dodavanje sile
+        rb.AddForce (movement * speed);
+    }
+
+     void OnTriggerEnter(Collider other)
+    {
+        //provjera
+        if (other.gameObject.CompareTag("Pick Up"))
+        {
+            //objekt
+            other.gameObject.SetActive(false);
+
+            //nakon pov
+            //povecava za 1
+            zbroj += 1;
+            SetCountText();
+        }
+    }
+
+    void SetCountText ()
+    {
+        countText.text = "Rezultat: " + zbroj.ToString();
+    }
+}
